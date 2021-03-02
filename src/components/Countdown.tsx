e7eaf0599925d3e480db245763f67009f41b90da
Danilo D. Vaz <danilodvaz@gmail.com>
2021-03-01 23:15:48 -0300

    Substituido valores fixos pelos contextos

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Countdown.module.css';
 
 // Adicionando a tipagem 
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
+    const { startNewChallenge } = useContext(ChallengesContext);
+
     const [time, setTime] = useState(25 * 60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
@@ -34,6 +37,7 @@ export function Countdown() {
         } else if (isActive && time === 0) {
             setHasFinished(true);
             setIsActive(false);
+            startNewChallenge();
         }
     }, [isActive, time])
 
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -4,28 +4,77 @@
 // Todo componente que estiver dentro do contexto, terá acesso aos dados, bastar utilizar o 
 // useContext e importar este arquivo de contexto.
 import { createContext, ReactNode, useState } from 'react';
+import challenges from '../../challenges.json';
 
-export const ChallengesContext = createContext({});
+interface Challenge {
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
+
+interface ChallengesContextData {
+    level: number;
+    currentExperience: number;
+    challengesCompleted: number;
+    activeChallenge: Challenge;
+    experienceToNextLevel: number;
+    levelUp: () => void;
+    startNewChallenge: () => void;
+    resetChallenge: () => void;
+}
 
 interface ChallengesProviderProps {
     // Como o children é um componente react, pode ser tipado como ReactNode, que aceita qualquer elemento filho
     children: ReactNode
 }
 
+// Cria o contexto e define o formato
+export const ChallengesContext = createContext({} as ChallengesContextData);
+
 export function ChallengesProvider({children}: ChallengesProviderProps) {
     const [level, setLevel] = useState(1);
     const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState(0);
 
+    const [activeChallenge, setActiveChallenge] = useState(null);
+
+    const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
     function levelUp() {
         setLevel(level + 1);
+    }
+
+    function startNewChallenge() {
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+        const challenge = challenges[randomChallengeIndex];
+
+        setActiveChallenge(challenge);
+    }
+
+    function resetChallenge() {
+        setActiveChallenge(null);
     }
 
     return (
         // Todo componente que ficar dentro do contexto, poderá acessar as informações.
         // Foi adicionado neste arquivo, pois praticamente todos os componentes irão compartilhar
-        <ChallengesContext.Provider value={{ level, currentExperience, challengesCompleted, levelUp }}>
+        <ChallengesContext.Provider
+            value={{
+                level,
+                currentExperience,
+                challengesCompleted,
+                experienceToNextLevel,
+                levelUp,
+                startNewChallenge,
+                activeChallenge,
+                resetChallenge
+            }}
+        >
             {children}
         </ChallengesContext.Provider>
     );
 }
+
+
+
+48
