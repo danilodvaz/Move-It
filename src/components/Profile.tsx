6b340ebb406c618ddef9f6f7e707004f9100472e
Danilo D. Vaz <danilodvaz@gmail.com>
2021-03-22 23:12:52 -0300

    Atualizado nome

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -9,7 +9,7 @@ export function Profile() {
         <div className={styles.profileContainer}>
             <img src="https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="Doguinho"/>
             <div>
-                <strong>Danilo Domingues</strong>
+                <strong>Doguinho</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
                     Level {level}
